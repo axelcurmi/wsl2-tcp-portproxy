32ba1853457ac8d01006c6980d1200e0abea1b54
Axel Curmi <axel.curmi@gmail.com>
2022-02-08 12:16:45 +0100

    Add uninstall mode

--- wsl2-portproxy.ps1
+++ wsl2-portproxy.ps1
@@ -1,9 +1,3 @@
-# Settings
-$firewall_rule_name = "WSL2 Firewall";
-$ports = @("80", "8000", "8080", "4240-4249");
-
-$wsl_ip = bash.exe -c "ip addr show eth0 | grep 'inet\b' | xargs | cut -d' ' -f2 | cut -d'/' -f1";
-
 function DeletePortProxy {
     param (
         $ListenAddress,
@@ -23,6 +17,25 @@ function AddPortProxy {
     netsh interface portproxy add v4tov4 listenaddress=$ListenAddress listenport=$ListenAndConnectPort connectaddress=$ConnectAddress connectport=$ListenAndConnectPort;
 }
 
+# Settings
+$install = $true;
+$firewall_rule_name = "WSL2 Firewall";
+$ports = @("8000", "8080", "4240-4249");
+
+$wsl_ip = bash.exe -c "ip addr show eth0 | grep 'inet\b' | xargs | cut -d' ' -f2 | cut -d'/' -f1";
+
+if ($args[0] -ne $null) {
+    if($args[0] -eq "--uninstall" -or $args[0] -eq "-u") {
+        Write-Host "[+] Operating in uninstall mode.";
+        $install = $false;
+    } else {
+        Write-Warning "Usage: wsl1-portproxy.ps1 [--uninstall | -u]";
+        exit 1;
+    }
+} else {
+    Write-Host "[+] Operating in install mode.";
+}
+
 # Remove Firewall Exception Rules
 try {
     Remove-NetFireWallRule -DisplayName $firewall_rule_name -ErrorAction Stop;
@@ -33,9 +46,11 @@ try {
     exit 1;
 }
 
-# Add Firewall Rules
-New-NetFireWallRule -DisplayName $firewall_rule_name -Direction Outbound -LocalPort $ports -Action Allow -Protocol TCP;
-New-NetFireWallRule -DisplayName $firewall_rule_name -Direction Inbound  -LocalPort $ports -Action Allow -Protocol TCP;
+# Add Firewall Rules (install only)
+if ($install) {
+    New-NetFireWallRule -DisplayName $firewall_rule_name -Direction Outbound -LocalPort $ports -Action Allow -Protocol TCP;
+    New-NetFireWallRule -DisplayName $firewall_rule_name -Direction Inbound  -LocalPort $ports -Action Allow -Protocol TCP;
+}
 
 foreach ($port in $ports) {
     if ($port.Contains("-")) {
@@ -45,10 +60,14 @@ foreach ($port in $ports) {
 
         for ($i = $from; $i -le $to; $i++) {
             DeletePortProxy -ListenAddress "*" -ListenPort $i;
-            AddPortProxy -ListenAddress "*" -ConnectAddress $wsl_ip -ListenAndConnectPort $i;
+            if ($install) {
+                AddPortProxy -ListenAddress "*" -ConnectAddress $wsl_ip -ListenAndConnectPort $i;
+            }
         }
     } else {
         DeletePortProxy -ListenAddress "*" -ListenPort $port;
-        AddPortProxy -ListenAddress "*" -ConnectAddress $wsl_ip -ListenAndConnectPort $port;
+        if ($install) {
+            AddPortProxy -ListenAddress "*" -ConnectAddress $wsl_ip -ListenAndConnectPort $port;
+        }
     }
 }
